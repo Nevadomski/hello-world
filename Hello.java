a82705c49fd671fa318bb160744b8e50bb4e4107
Nevadomski <84872233+Nevadomski@users.noreply.github.com>
2021-05-26 15:00:54 -0400

    Adding lab 1 file

---
+++ Hello.java
@@ -0,0 +1,18 @@
+/********************************************************************************************
+* Hello
+*********************************************************************************************
+* Brent Nevadomski
+* 5/26/2021
+* CMSC 255 C01
+********************************************************************************************/
+
+public class Hello {
+	public static void main(String[] args) {
+		System.out.println("Brent Nevadomski");
+		System.out.println("Hello");
+		System.out.println("Lab 1 Task 4");
+		System.out.println("5/26/2021");
+		System.out.println("CMSC 255 C01");
+		System.out.println();
+	}
+}
